4049848dd9a70c811a071abbbc35028e5132b9d5
Aniol Cabrespina <63777176+Sttratto@users.noreply.github.com>
2020-09-22 13:14:07 +0200

    Patrol v1

---
+++ Assets/Scripts/IA/enemyContacts.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class enemyContacts : MonoBehaviour
+{
+    [Header("Transforms")]
+    [SerializeField]
+    Transform wall;
+    [SerializeField]
+    Transform nextStep;
+    [SerializeField]
+    Transform playerContact;
+    [SerializeField]
+    Transform playerBehind;
+    [SerializeField]
+    float distance;
+    [SerializeField]
+    LayerMask whatIsGround;
+    [SerializeField]
+    LayerMask whatIsPlayer;
+
+    [Header("Contacts")]
+    public bool canNextStep;
+    public bool wallContact;
+    public bool playerContactHit;
+    public bool playerBehindHit;
+    private void Update()
+    {
+        RaycastHit2D nextHit = Physics2D.Raycast(nextStep.position, nextStep.right, distance, whatIsGround);
+        canNextStep = nextHit;
+
+        RaycastHit2D nextWall = Physics2D.Raycast(wall.position, wall.right, distance, whatIsGround);
+        wallContact = nextWall;
+
+        RaycastHit2D nextPlayer = Physics2D.Raycast(playerContact.position, playerContact.right, distance, whatIsPlayer);
+        playerContactHit = nextPlayer;
+
+        RaycastHit2D closePlayer = Physics2D.Raycast(playerBehind.position, playerBehind.right * -1, distance * 5, whatIsPlayer);
+        playerBehindHit = closePlayer;
+    }
+
+    private void OnDrawGizmos()
+    {
+        Gizmos.DrawRay(nextStep.position, nextStep.right * distance);
+        Gizmos.DrawRay(wall.position, wall.right * distance);
+        Gizmos.DrawRay(playerContact.position, playerContact.right * distance);
+        Gizmos.DrawRay(playerBehind.position, playerBehind.right * -1 * distance * 5);
+    }
+}
---
+++ Assets/Scripts/IA/enemyPatrol.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class enemyPatrol : MonoBehaviour
+{
+    Rigidbody2D rb2d;
+    enemyContacts enemyContacts;
+
+    [SerializeField]
+    float speed;
+
+    void Start()
+    {
+        rb2d = GetComponent<Rigidbody2D>();
+        enemyContacts = GetComponent<enemyContacts>();
+    }
+
+
+    void Update()
+    {
+        if (enemyContacts.wallContact || !enemyContacts.canNextStep || enemyContacts.playerBehindHit)
+        {
+            transform.Rotate(new Vector3(0.0f, 180.0f, 0.0f));
+        }
+        rb2d.velocity = new Vector2(Time.fixedDeltaTime * speed * transform.right.x, rb2d.velocity.y);
+    }
+}
