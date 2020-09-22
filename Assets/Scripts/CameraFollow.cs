eb886b324fa6b9a43f7b5efbdc2ef75ee98144f8
Arnau Reig <arreme@hotmail.es>
2020-09-23 01:02:13 +0200

    no message

--- Assets/Scripts/CameraFollow.cs
+++ Assets/Scripts/CameraFollow.cs
@@ -8,19 +8,37 @@ public class CameraFollow : MonoBehaviour
     [Header("Camera Settings")]
     [SerializeField] private float clampSpeed;
     [SerializeField] private Vector3 offset;
+    private 
     void Start()
     {
         gameObject.transform.position = trm.position;
+        Retreat.current.onTimeFinish += zoomIn;
     }
 
     void FixedUpdate()
     {
         Vector3 finalPos = trm.position + offset;
         transform.position = Vector3.Lerp(transform.position, finalPos, clampSpeed);
+        if (offset.z >= -10)
+        {
+            CancelInvoke();
+        }
     }
 
     public void ChangeTarget(GameObject gameObj)
     {
         trm = gameObj.transform;
+    }
+
+    private void zoomIn()
+    {
+        InvokeRepeating("actualZoomIn", 0, 3);
+    }
+
+    private void actualZoomIn()
+    {
+        Debug.Log("Hey");
+        offset = new Vector3(offset.x + 0.00015f,offset.y-0.002f, offset.z + 0.035f);
+        
     }
 }
