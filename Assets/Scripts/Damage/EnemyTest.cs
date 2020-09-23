b4fa8b62f2844de1de7506d7b994f850977227e7
Adria <adriapujolsierra@gmail.com>
2020-09-23 16:56:48 +0200

    ObjPooler Done It

---
+++ Assets/Scripts/Damage/EnemyTest.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyTest : MonoBehaviour, IPooledObj
+{
+    public float upForce = 10f;
+    public float sideForce = 1f;
+
+    public void OnObjSpawned()
+    {
+        float xForce = Random.Range(-sideForce, sideForce);
+        float yForce = Random.Range(upForce / 2f, upForce);
+
+        Vector2 force = new Vector2(xForce, yForce);
+
+        GetComponent<Rigidbody2D>().velocity = force;
+    }
+}
