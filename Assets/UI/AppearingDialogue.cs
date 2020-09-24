8d7d15360d27e0018c5c63c2a267c06274e061fb
Adria <adriapujolsierra@gmail.com>
2020-09-24 16:08:44 +0200

    DialogueTrigger done it

---
+++ Assets/UI/AppearingDialogue.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class AppearingDialogue : MonoBehaviour
+{
+    [SerializeField] private Image customImage;
+    [SerializeField] private TextMeshProUGUI customText;
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Player"))
+        {
+            customImage.enabled = true;
+            customText.enabled = true;
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Player"))
+        {
+            customImage.enabled = false;
+            customText.enabled = false;
+        }
+
+    }
+}
