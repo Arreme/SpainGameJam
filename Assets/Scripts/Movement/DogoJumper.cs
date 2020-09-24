158cbfcdfa409e5935abb89564ac2b2387b3dbfc
Aniol Cabrespina <63777176+Sttratto@users.noreply.github.com>
2020-09-24 13:52:13 +0200

    Dogo Runs ans Stops

---
+++ Assets/Scripts/Movement/DogoJumper.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DogoJumper : MonoBehaviour
+{
+    [SerializeField]
+    private float JumpForce = 10f;
+
+    public float getForce()
+    {
+        return JumpForce;
+    }
+}
