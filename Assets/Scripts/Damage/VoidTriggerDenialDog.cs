87cfd5e3a891c41964978b77b9f4d88a3f7d8f70
Aniol Cabrespina <63777176+Sttratto@users.noreply.github.com>
2020-09-25 17:42:38 +0200

    DenialAlmostFinished

--- Assets/Scripts/Damage/VoidTriggerDenialDog.cs
+++ Assets/Scripts/Damage/VoidTriggerDenialDog.cs
@@ -8,13 +8,25 @@ public class VoidTriggerDenialDog : MonoBehaviour
     Transform dogRespawn;
     [SerializeField]
     Transform darknessRespawn;
+    [SerializeField]
+    bool darkness;
+
     private void OnTriggerEnter2D(Collider2D collision)
     {
         if (collision.gameObject.layer == LayerMask.NameToLayer("Player"))
         {
             collision.gameObject.GetComponent<PlayerHealthSystem>().Kill();
             GameObject.FindGameObjectWithTag("Dog").transform.position = dogRespawn.transform.position;
-            GameObject.FindGameObjectWithTag("darkness").transform.position = darknessRespawn.transform.position;
+            GameObject.FindGameObjectWithTag("Dog").GetComponent<Rigidbody2D>().velocity = new Vector2(0, 0);
+            if (darkness)
+            {
+                GameObject.FindGameObjectWithTag("darkness").transform.position = darknessRespawn.transform.position;
+                GameObject.FindGameObjectWithTag("darkness").GetComponent<DarknessFades>().vanish();
+                GameObject.FindGameObjectWithTag("darkness").GetComponent<DarknessFades>().InvokeRepeating("fadeIn", 0, 0.1f);
+                GameObject.FindGameObjectWithTag("darknessEnlarger").GetComponent<DarknessFades>().vanish();
+                GameObject.FindGameObjectWithTag("darknessEnlarger").GetComponent<DarknessFades>().InvokeRepeating("fadeIn", 0, 0.1f);
+            }
+            
         }
     }
 }
---
+++ Assets/Scripts/Movement/DarknessFades.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DarknessFades : MonoBehaviour
+{
+    SpriteRenderer sprite;
+    Color color;
+    private void Start()
+    {
+        sprite = GetComponent<SpriteRenderer>();
+        color = sprite.color;
+    }
+    public void vanish()
+    {
+        color.a = 0f;
+    }
+
+    private void fadeIn()
+    {
+        color.a += 0.05f;
+        sprite.color = color;
+        if (color.a == 1f)
+            StopAllCoroutines();
+    }
+}
