1a71c9892cbcc0debdcb3a8c8567b876f0142c68
Aniol Cabrespina <63777176+Sttratto@users.noreply.github.com>
2020-09-28 11:50:32 +0200

    BossHealth

---
+++ Assets/Scripts/Damage/BossHealth.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class BossHealth : Damageable
+{
+    public Slider slider;
+    public float sliderSmooth = 0.15f;
+
+    new private void Awake()
+    {
+        base.Awake();
+        slider.maxValue = maxHealth;
+        slider.value = slider.maxValue;
+    }
+
+    private void FixedUpdate()
+    {
+        SliderUpdate();
+    }
+
+    public override void Damage(float amount)
+    {
+        base.Damage(amount);
+    }
+
+    private void SliderUpdate()
+    {
+        slider.value = Mathf.Lerp(slider.value, currentHealth, sliderSmooth);
+    }
+
+    public override void Kill()
+    {
+        base.Kill();
+    }
+}
